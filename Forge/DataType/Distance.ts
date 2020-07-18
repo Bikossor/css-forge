376a2473c69ef9f6eb700eab83ab517b161886fe
Bikossor <andre.lichtenthaeler@gmail.com>
2020-07-18 13:46:27 +0200

    Closes #3

--- Forge/DataType/Distance.ts
+++ Forge/DataType/Distance.ts
@@ -1,86 +1,158 @@
+import { IDistance } from "../Interface/IDistance";
+
 /**
  * Font size of the element.
  * @param value 
  */
-export const em = (value: number): string => `${value}em`;
+export const em = (value: number): IDistance => {
+    return {
+        value,
+        unit: 'em',
+    };
+};
 
 /**
  * x-height of the element's font.
  * @param value 
  */
-export const ex = (value: number): string => `${value}ex`;
+export const ex = (value: number): IDistance => {
+    return {
+        value,
+        unit: 'ex',
+    };
+};
 
 /**
  * Cap height (the nominal height of capital letters) of the element's font.
  * @param value 
  */
-export const cap = (value: number): string => `${value}cap`;
+export const cap = (value: number): IDistance => {
+    return {
+        value,
+        unit: 'cap',
+    };
+};
 
 /**
  * Average character advance of a narrow glyph in the element’s font, as represented by the “0” (ZERO, U+0030) glyph.
  * @param value 
  */
-export const ch = (value: number): string => `${value}ch`;
+export const ch = (value: number): IDistance => {
+    return {
+        value,
+        unit: 'ch',
+    };
+};
 
 /**
  * Average character advance of a full width glyph in the element’s font, as represented by the “水” (CJK water ideograph, U+6C34) glyph.
  * @param value 
  */
-export const ic = (value: number): string => `${value}ic`;
+export const ic = (value: number): IDistance => {
+    return {
+        value,
+        unit: 'ic',
+    };
+};
 
 /**
  * Font size of the root element.
  * @param value 
  */
-export const rem = (value: number): string => `${value}rem`;
+export const rem = (value: number): IDistance => {
+    return {
+        value,
+        unit: 'rem',
+    };
+};
 
 /**
  * Line height of the element.
  * @param value 
  */
-export const lh = (value: number): string => `${value}lh`;
+export const lh = (value: number): IDistance => {
+    return {
+        value,
+        unit: 'lh',
+    };
+};
 
 /**
  * Line height of the root element.
  * @param value 
  */
-export const rlh = (value: number): string => `${value}rlh`;
+export const rlh = (value: number): IDistance => {
+    return {
+        value,
+        unit: 'rlh',
+    };
+};
 
 /**
  * 1% of viewport's width.
  * @param value 
  */
-export const vw = (value: number): string => `${value}vw`;
+export const vw = (value: number): IDistance => {
+    return {
+        value,
+        unit: 'vw',
+    };
+};
 
 /**
  * 1% of viewport's height.
  * @param value 
  */
-export const vh = (value: number): string => `${value}vh`;
+export const vh = (value: number): IDistance => {
+    return {
+        value,
+        unit: 'vh',
+    };
+};
 
 /**
  * 1% of viewport's size in the root element's inline axis.
  * @param value 
  */
-export const vi = (value: number): string => `${value}vi`;
+export const vi = (value: number): IDistance => {
+    return {
+        value,
+        unit: 'vi',
+    };
+};
 
 /**
  * 1% of viewport's size in the root element's block axis.
  * @param value 
  */
-export const vb = (value: number): string => `${value}vb`;
+export const vb = (value: number): IDistance => {
+    return {
+        value,
+        unit: 'vb',
+    };
+};
 
 /**
  * 1% of viewport's smaller dimension.
  * @param value 
  */
-export const vmin = (value: number): string => `${value}vmin`;
+export const vmin = (value: number): IDistance => {
+    return {
+        value,
+        unit: 'vmin',
+    };
+};
 
 /**
  * 1% of viewport's larger dimension.
  * @param value 
  */
-export const vmax = (value: number): string => `${value}vmax`;
+export const vmax = (value: number): IDistance => {
+    return {
+        value,
+        unit: 'vmax',
+    };
+};
 
 
 
@@ -89,40 +161,75 @@ export const vmax = (value: number): string => `${value}vmax`;
  * Centimeters (1cm = 96px/2.54)
  * @param value 
  */
-export const cm = (value: number): string => `${value}cm`;
+export const cm = (value: number): IDistance => {
+    return {
+        value,
+        unit: 'cm',
+    };
+};
 
 /**
  * Millimeters (1mm = 1/10th of 1cm)
  * @param value 
  */
-export const mm = (value: number): string => `${value}mm`;
+export const mm = (value: number): IDistance => {
+    return {
+        value,
+        unit: 'mm',
+    };
+};
 
 /**
  * Quarter-millimeters (1Q = 1/40th of 1cm)
  * @param value 
  */
-export const Q = (value: number): string => `${value}Q`;
+export const Q = (value: number): IDistance => {
+    return {
+        value,
+        unit: 'Q',
+    };
+};
 
 /**
  * Inches (1inch = 2.54cm = 96px)
  * @param value 
  */
-export const inch = (value: number): string => `${value}in`;
+export const inch = (value: number): IDistance => {
+    return {
+        value,
+        unit: 'inch',
+    };
+};
 
 /**
  * Picas (1pc = 1/16th of 1inch)
  * @param value 
  */
-export const pc = (value: number): string => `${value}pc`;
+export const pc = (value: number): IDistance => {
+    return {
+        value,
+        unit: 'pc',
+    };
+};
 
 /**
  * Points (1pt = 1/72th of 1inch)
  * @param value 
  */
-export const pt = (value: number): string => `${value}pt`;
+export const pt = (value: number): IDistance => {
+    return {
+        value,
+        unit: 'pt',
+    };
+};
 
 /**
  * Pixels (1px = 1/96th of 1inch)
  * @param value 
  */
-export const px = (value: number): string => `${value}px`;
+export const px = (value: number): IDistance => {
+    return {
+        value,
+        unit: 'px',
+    };
+};
